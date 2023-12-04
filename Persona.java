1b21c80e268976cc06822c3b8985e80f6943719b
Jordan Urzua <jordanpukuu@gmail.com>
2023-12-04 18:49:24 -0300

    update

--- Persona.java
+++ Persona.java
@@ -17,4 +17,56 @@ public class Persona {
         this.telefono = telefono;
         this.email = email;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan Perez", "555555555", "juan@gmail.com");
+        System.out.println(persona1);
+        System.out.println(numeroPersonas);
+    }
+
+
+
 }
